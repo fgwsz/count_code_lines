23af1e73d01446d46304cc4a0177587a4b2624eb
fgwsz <3191000458@qq.com>
2025-01-09 17:29:03 +0800

    rewrite count_lines_in_file

--- source/count_lines_in_file.hpp
+++ source/count_lines_in_file.hpp
@@ -1,24 +1,43 @@
 #pragma once
 #include<cstddef>//std::size_t
+#include<cstdio>//std::fopen
 #include<filesystem>//std::filesystem
-#include<fstream>//std::ifstream
-#include<string>//std::getline
-#include<vector>//std::vector
 #include"is_cpp_file.hpp"
 // 统计文件的行数
 std::size_t count_lines_in_file(std::filesystem::path const& file_path){
     if(!is_cpp_file(file_path)){
         return 0;
     }
-    std::ifstream file(file_path);
-    if(!file.is_open()){
-        std::cerr<<"Error opening file: "<<file_path<<std::endl;
+    static char buffer[1024]={};
+    std::FILE* file=std::fopen(
+        std::filesystem::absolute(file_path).string().c_str()
+        ,"rb"
+    );
+    if(!file){
+        std::fprintf(
+            stderr,"Error: fopen(\"%s\") error!\n"
+            ,std::filesystem::absolute(file_path).string().c_str()
+        );
         return 0;
     }
-    static std::string line={};
+    std::size_t count=0;
     std::size_t lines=0;
-    while(std::getline(file,line)){
-        lines++;
+    std::size_t index=0;
+    while((count=fread(buffer,sizeof(char),sizeof(buffer),file))>0){
+        for(index=0;index<count;++index){
+            if(buffer[index]=='\n'){
+                ++lines;
+            }
+        }
     }
+    if(ferror(file)){
+        std::fprintf(
+            stderr,"Error: fread(\"%s\") error!\n"
+            ,std::filesystem::absolute(file_path).string().c_str()
+        );
+        std::fclose(file);
+        return 0;
+    }
+    std::fclose(file);
     return lines;
 }
